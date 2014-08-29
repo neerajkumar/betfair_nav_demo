a2056a0559578f60128ac2f237492899c35a545b
Neeraj Kumar <neeraj.kumar@gmail.com>
2014-08-29 17:40:34 +0530

    some refactoring in person.js file

--- app/assets/javascripts/views/person.js
+++ app/assets/javascripts/views/person.js
@@ -4,29 +4,11 @@ var templates = require('../templates');
 module.exports = View.extend({
     template: templates.includes.person,
     bindings: {
-        'model.fullName': '[role=name]',
-        'model.avatar': {
-            type: 'attribute',
-            role: 'avatar',
-            name: 'src'
-        },
-        'model.editUrl': {
-            type: 'attribute',
-            role: 'action-edit',
-            name: 'href'
-        },
         'model.viewUrl': {
             type: 'attribute',
             role: 'eventTypeName',
             name: 'href'
         },
         'model.eventType': '[role=eventTypeName]'
-    },
-    events: {
-        'click [role=action-delete]': 'handleRemoveClick'
-    },
-    handleRemoveClick: function () {
-        this.model.destroy();
-        return false;
     }
 });
