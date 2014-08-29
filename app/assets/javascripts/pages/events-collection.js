a8a1d3eff9054a9856e3c68d7ad6c6b89ee658b3
Neeraj Kumar <neeraj.kumar@gmail.com>
2014-08-29 17:38:23 +0530

    some refactoring in collection-demo

--- app/assets/javascripts/pages/events-collection.js
+++ app/assets/javascripts/pages/events-collection.js
@@ -8,29 +8,7 @@ module.exports = PageView.extend({
     bindings: {
         'model.childrenNames': '[role=childName]',
         'model.name': '[role=name]'
-//        'model.avatar': {
-//            type: 'attribute',
-//            role: 'avatar',
-//            name: 'src'
-//        },
-//        'model.editUrl': {
-//            type: 'attribute',
-//            role: 'edit',
-//            name: 'href'
-//        }
     },
-//    render: function(){
-//        this.renderWithTemplate();
-//        this.renderCollection(this.collection, EventView, this.getByRole('events-list'));
-//        if (!this.collection.length) {
-//            this.fetchCollection();
-//        }
-//    },
-//
-//    fetchCollection: function () {
-//        this.collection.fetch();
-//        return false;
-//    },
     initialize: function (spec) {
         var self = this;
         app.people.getOrFetch(spec.id, function (err, model) {
