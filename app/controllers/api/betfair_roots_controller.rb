e62575a5569aa94be245874ad1812ea1856615f2
Neeraj Kumar <neeraj.kumar@gmail.com>
2014-10-18 17:13:39 +0530

    some refactoring

--- app/controllers/api/betfair_roots_controller.rb
+++ app/controllers/api/betfair_roots_controller.rb
@@ -3,11 +3,11 @@ class Api::BetfairRootsController < ApplicationController
   # GET /betfair_roots
   # GET /betfair_roots.json
   def index
-    $betfair_roots = Betfair.new.data
+    betfair_roots = Betfair.new.data
 
     respond_to do |format|
       format.json do
-        render json: $betfair_roots.to_json
+        render json: betfair_roots.to_json
       end
     end
   end
